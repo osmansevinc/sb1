ffa11fb51e803f0f62495f5db4069dc5ab6b3c02
Osman Sevinc <osmansevinc4@gmail.com>
2024-10-30 17:46:54 +0300

    last changes of gcp

--- src/main/java/com/streamsegmenter/service/impl/GcpStorageService.java
+++ src/main/java/com/streamsegmenter/service/impl/GcpStorageService.java
@@ -1,13 +1,22 @@
 package com.streamsegmenter.service.impl;
 
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Cors;
+import com.google.cloud.storage.HttpMethod;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.ImmutableList;
 import com.streamsegmenter.config.StorageConfig;
 import com.streamsegmenter.service.StorageService;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.CompletableFuture;
@@ -22,11 +31,43 @@ public class GcpStorageService implements StorageService {
     public GcpStorageService(StorageConfig config) {
         this.bucketName = config.getGcpBucket();
         this.projectId = config.getGcpProjectId();
-        this.storage = StorageOptions.newBuilder()
-            .setProjectId(projectId)
-            .build()
-            .getService();
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream serviceAccountStream = classLoader.getResourceAsStream("plasma-envoy-440206-j7-e9f5b61bf9f8.json")) {
+            if (serviceAccountStream == null) {
+                throw new FileNotFoundException("Service account key file not found in resources folder");
+            }
+            this.storage = StorageOptions.newBuilder()
+                    .setProjectId(projectId)
+                    .setCredentials(ServiceAccountCredentials.fromStream(serviceAccountStream))
+                    .build()
+                    .getService();
+            configureBucketCors(bucketName, "Content-Type", 3600);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
+
+    private void configureBucketCors(String bucketName, String responseHeader, Integer maxAgeSeconds) {
+        Bucket bucket = storage.get(bucketName);
+
+        Cors cors = Cors.newBuilder()
+                .setOrigins(ImmutableList.of(Cors.Origin.of("*")))  // Allow all origins
+                .setMethods(ImmutableList.of(HttpMethod.GET))
+                .setResponseHeaders(ImmutableList.of(responseHeader))
+                .setMaxAgeSeconds(maxAgeSeconds)
+                .build();
+
+        // Update the bucket with the new CORS configuration
+        bucket.toBuilder().setCors(ImmutableList.of(cors)).build().update();
+
+        log.info("Bucket {} was updated with a CORS config to allow GET requests from all origins sharing {} responses across origins",
+                bucketName, responseHeader);
+    }
+
 
     @Override
     public CompletableFuture<String> uploadSegment(Path segmentPath, String streamId) {
